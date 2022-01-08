51153fb5be7de343304ed5a501b05383a7cb8892
jason-moonKor <jaesangv@naver.com>
2022-01-08 20:32:47 +0900

    ver 0.3

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
 	<div>
-		<h1>하이 {{ animal }}</h1>
-		<h2>원숭이는 {{ food }} 를 좋아합니다</h2>
-		<input type="text" v-model="food" />
-		<div v-html="alertMsg"></div>
-		<div v-html="subHtml"></div>
-		<img :src="imgSrc" alt="random" />
-		<a :href="food">{{ food }}</a>
-		<hr />
-		<h2 :class="{red: food === 'apple', 'not-good': food === `rice`}">
-			원숭이는 {{ food }} 를 좋아합니다
-		</h2>
-		<h2>원숭이는 {{ food }} 를 좋아합니다</h2>
+		<!-- <h2>당신의 나이는 {{ age }} 입니다</h2>
+		<h3 v-if="age > 18">당신은 성인입니다</h3>
+		<h3 v-else-if="13 < age && age <= 18">당신은 청소년 입니다</h3>
+		<h3 v-else>당신은 어른이 아닙니다</h3> -->
+
+		<!-- <h2 v-if="display">보입니다</h2>
+		<h2 v-show="display">보입니다</h2> -->
+		<template v-for="(a, idx) in animals" :key="idx">
+			<span v-if="a !== 'monkey'">{{ a }} 인덱스번호: {{ idx }}</span>
+			<h2 v-if="a !== 'monkey'"></h2>
+		</template>
+		<!-- <ul>
+			<li v-for="(u, i) in users" :key="i">
+				이름은 : {{ u.name }} 직업 : {{ u.job }} 인덱스 번호 = {{ i }}
+				<p v-for="i in u.skill" :key="i">{{ i }}</p>
+			</li>
+		</ul> -->
 	</div>
 </template>
 
@@ -20,12 +25,29 @@ export default {
 	name: "App",
 	data() {
 		return {
-			animal: "monkey",
-			food: "apple",
-			alertMsg: "<h2>경고</h2>",
-			subHtml: `<button onclick="document.querySelector('body').style.backgroundColor = 'beige'">클릭</button>`,
-			imgSrc: "http://placeimg.com/200/200/any",
-			naverUrl: "https://www.naver.com"
+			age: 11,
+			display: false,
+			animals: ["monkey", "rat", "dog", "lion"],
+			users: [
+				{
+					name: "jason",
+					job: "developer",
+					gender: "male",
+					skill: ["html", "javascript", "css"]
+				},
+				{
+					name: "JAY",
+					job: "cook",
+					gender: "male",
+					skill: ["HTML", "JAVA", "JASONSON"]
+				},
+				{
+					name: "FUCK",
+					job: "cooker",
+					gender: "female",
+					skill: ["hhtttmmmll", "javaScript", "cSS"]
+				}
+			]
 		};
 	}
 };
---
+++ src/App1.vue
@@ -0,0 +1,59 @@
+<template>
+	<div>
+		<h1>하이 {{ animal }}</h1>
+		<h2>원숭이는 {{ food }} 를 좋아합니다</h2>
+		<input type="text" v-model="food" />
+		<div v-html="alertMsg"></div>
+		<div v-html="subHtml"></div>
+		<img :src="imgSrc" alt="random" />
+		<br />
+		<a :href="food">{{ food }}</a>
+		<hr />
+		<h2 :class="{red: food === 'apple', 'not-good': food === `rice`}">
+			원숭이는 {{ food }} 를 좋아합니다
+		</h2>
+		<h2>원숭이는 {{ food }} 를 좋아합니다</h2>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "App",
+	data() {
+		return {
+			animal: "monkey",
+			food: "apple",
+			alertMsg: "<h2>경고</h2>",
+			subHtml: `<button onclick="document.querySelector('body').style.backgroundColor = 'beige'">클릭</button>`,
+			imgSrc: "http://placeimg.com/700/300/any",
+			naverUrl: "https://www.naver.com"
+		};
+	}
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+input {
+	font-size: 20px;
+}
+.orange {
+	color: orange;
+}
+.salmon {
+	color: salmon;
+}
+.red {
+	color: red;
+}
+.not-good {
+	text-decoration: line-through;
+}
+</style>
